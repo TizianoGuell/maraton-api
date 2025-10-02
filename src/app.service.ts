ab28823e37b02cb96ee8a972bd950f50dd17937e
juan ignacio <salcedojuanignacio4@gmail.com>
2025-10-01 21:42:10 -0300

    el tizi se la come

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return '<h1></h1>';
+    return '<h1>El Tizi se la Come</h1>';
   }
 }
